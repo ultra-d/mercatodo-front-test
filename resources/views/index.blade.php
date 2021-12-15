bf3ff09dfe9d7c03d25a372e3350789c4c9cd189
Daniela Melo <ultradan@protonmail.com>
2021-12-15 17:24:15 -0500

    trying to style index

---
+++ resources/views/index.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.main')
+
+@section('content')
+    <h1>Hello from INDEX</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
 
-Route::view('/home', 'home')->middleware(['auth', 'verified']);
+Route::view('/home', 'home')->middleware(['auth', 'verified'])->name('home');
